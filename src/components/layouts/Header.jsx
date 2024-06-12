607bb8ec2fecc5dcbc9e9d9193aecdc5ce15fbaf
wsk0715 <leindey@gmail.com>
2024-06-12 23:58:24 +0900

    feat: 헤더 하이퍼링크 수정

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,17 +1,19 @@
 export default function Header() {
 	return (
-		<header className="flex items-center justify-between w-full h-24 p-4 bg-gray-700 text-white">
-			<div className="text-2xl font-bold">My Company</div>
+		<header className="flex items-center justify-between
+											 w-full h-24
+											 p-4
+											 bg-gray-700 text-white">
+			<div className="text-2xl font-bold">
+				<a href="/" className="hover:text-gray-400">My Company</a>
+			</div>
 			<nav>
-				<ul className="flex space-x-4">
+				<ul className="flex space-x-8">
 					<li>
-						<a href="/" className="hover:text-gray-400">Home</a>
+						<a href="/register" className="hover:text-gray-400">Register</a>
 					</li>
 					<li>
-						<a href="#" className="hover:text-gray-400">About</a>
-					</li>
-					<li>
-						<a href="#" className="hover:text-gray-400">Contact</a>
+						<a href="/members/admin" className="hover:text-gray-400">My Page</a>
 					</li>
 				</ul>
 			</nav>
