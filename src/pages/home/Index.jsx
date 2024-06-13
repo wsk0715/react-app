833ae74b75118b89146b74f1b18390b839724b48
wsk0715 <leindey@gmail.com>
2024-06-13 19:35:22 +0900

    test: 테스트를 위해 아이디 입력받아 세션에 저장하는 기능 추가

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 export default function Home() {
+	useEffect(() => {
+		sessionStorage.removeItem('id');
+		sessionStorage.setItem('id', window.prompt('(test) 아이디를 입력해주세요:'));
+	}, []);
+
 	return (
 		<div>
 			<h1 className="text-2xl">Home</h1>
