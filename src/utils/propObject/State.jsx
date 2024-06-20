98fc36459972102abb75cb4d5cdfcf17694ffe2c
wsk0715 <leindey@gmail.com>
2024-06-20 20:25:05 +0900

    refactor: 커스텀 훅 수정

--- src/utils/propObject/State.jsx
+++ src/utils/propObject/State.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
 const State = (name) => {
-	const state = useState('');
+  const [value, setter] = useState('');
 
-	return {
-		name: name,
-		value: state[0],  // getter
-		set: (value) => {
-			state[1](value);  // setter
-		}
-	}
+  return {
+    name: name,
+    value: value,
+    set: (value) => {
+      setter(value);
+    }
+  }
 }
 
 export default State;
