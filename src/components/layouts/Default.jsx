5793edfd7212178bf2aafd22fa77ad5b60f0d33d
wsk0715 <leindey@gmail.com>
2024-06-20 20:52:29 +0900

    design: 레이아웃 크기 수정

--- src/components/layouts/Default.jsx
+++ src/components/layouts/Default.jsx
@@ -6,7 +6,10 @@ export default function LayoutDefault({ children }) {
 		<div id="body" className="flex flex-col min-h-screen ">
 			<Header />
 			<main className="flex flex-grow flex-col">
-				<section className="flex flex-grow mx-8 p-16 pt-12 bg-gray-50">
+				<section id="mainSection"
+								 className="flex flex-grow
+								 						min-w-[28rem]
+								 						mx-[4vw] p-16 pt-12 bg-gray-50">
 					{ children }
 				</section>
 			</main>
--- src/components/member/MemberForm.jsx
+++ src/components/member/MemberForm.jsx
@@ -1,8 +1,9 @@
 export default function MemberForm({ title, children }) {
 	return (
 		<div className="flex flex-col flex-grow items-center min-h-[28rem]">
-			<div className="flex-grow
-											w-[50%] max-w-[40rem] h-full
+			<div id="memberForm"
+					 className="flex-grow
+											w-[60%] max-w-[40rem] min-w-[20rem] h-full
 											p-8 border-2 rounded-md
 											bg-gray-100 text-center ">
 				<h1 className="w-full text-center my-4 text-3xl">{ title }</h1>
